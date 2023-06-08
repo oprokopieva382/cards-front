5eb32d90f23db601e567dcfe6db25f2bf7e62482
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-06-08 10:18:44 -0500

    updated base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://oprokopieva382.github.io/cards-front",
+  base: "/cards-front/",
   plugins: [react()],
   server: {
     open: true,
