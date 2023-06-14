5f5b738f98143fecc07ff9065aa6e0d38691d810
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-06-14 00:59:05 -0500

    created PackTable component

--- src/features/auth/components/Packs.tsx
+++ src/features/auth/components/Packs.tsx
@@ -1,11 +1,13 @@
 import { PackNavigator } from "../../../comon/components/PackNavigator"
 import { PackSettings } from "../../../comon/components/PackSettings"
+import { PackTable } from "./PackTable"
 
 export const Packs = () => {
   return (
     <>
       <PackNavigator title="Packs list" buttonTitle="Add new pack" />
       <PackSettings />
-         </>
+      <PackTable />
+    </>
   )
 }
